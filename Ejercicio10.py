7e5a43c2c17fa100756deb682b4c12dc6c76e8a6
Ernesto Cristales <ernestorcs@gmail.com>
2021-02-04 21:37:33 -0600

    tercera estrella

---
+++ Ejercicio10.py
@@ -0,0 +1,18 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    
+    for key, value in student_marks.items():
+        if query_name == key:
+            sum = 0
+            contador = 0
+            for i in value:
+                sum += i
+                contador += 1
+            promedio = sum/contador
+            print("{:.2f}".format(promedio))
