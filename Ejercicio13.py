d3ef55524c9efa820eeae15182378360ffd20c33
Ernesto Cristales <ernestorcs@gmail.com>
2021-02-06 22:58:12 -0600

    proceso para la cuarta estrella

---
+++ Ejercicio13.py
@@ -0,0 +1,17 @@
+import numpy
+import numpy as np
+n,m = map(int,input().split())
+
+e=np.zeros((n,m),int)
+c=np.zeros((n,m),int)
+for i in range(n):
+  e[i]=np.array(input().split(),int)
+for i in range(n):
+  c[i]=np.array(input().split(),int)  
+
+print(e+c)
+print(e-c)
+print(e*c)
+print(np.array(e/c,int))
+print(e%c)
+print(e**c)
---
+++ Ejercicio19.py
@@ -0,0 +1,3 @@
+ercs = input().split()
+x = int(ercs[0])
+print(eval(input()) == int(ercs[1]))
